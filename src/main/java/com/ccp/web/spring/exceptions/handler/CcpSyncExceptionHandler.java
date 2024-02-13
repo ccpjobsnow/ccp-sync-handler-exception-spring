5cdd93491e423f8386ef2b03e34b43ca136a1f18
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-02-12 23:30:53 -0300

    criação do handler genérico

---
+++ src/main/java/com/ccp/web/spring/exceptions/handler/CcpSyncExceptionHandler.java
@@ -0,0 +1,59 @@
+package com.ccp.web.spring.exceptions.handler;
+
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.ccp.constantes.CcpConstants;
+import com.ccp.decorators.CcpJsonRepresentation;
+import com.ccp.exceptions.process.CcpFlow;
+import com.ccp.fields.validations.exceptions.CcpJsonInvalid;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestControllerAdvice
+public class CcpSyncExceptionHandler {
+
+	public static Function<Throwable, CcpJsonRepresentation> genericExceptionHandler;
+ 
+	@ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY)
+	@ExceptionHandler({ CcpJsonInvalid.class })
+	public Map<String, Object> handle(CcpJsonInvalid e) {
+		return e.errors.content;
+	}
+
+	@ResponseBody
+	@ExceptionHandler({ CcpFlow.class })
+	public Map<String, Object> handle(CcpFlow e, HttpServletResponse res){
+		
+		res.setStatus(e.status);
+		
+		String message = e.getMessage();
+		
+		CcpJsonRepresentation result = CcpConstants.EMPTY_JSON.put("message", message);
+		
+		if(e.fields.length <= 0) {
+			return result.content;
+		}
+		
+		CcpJsonRepresentation subMap = e.values.getJsonPiece(e.fields);
+		
+		CcpJsonRepresentation putAll = result.putAll(subMap);
+		
+		return putAll.content;
+	}
+
+	@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler({ Throwable.class })
+	public Map<String, Object> handle(Throwable e) {
+		if(genericExceptionHandler == null) {
+			throw new RuntimeException("genericExceptionHandler must has an instance ", e);
+		}
+		return genericExceptionHandler.apply(e).content;
+	}
+}
