dd5ffa7beb21c0e827f289aef74f1a2cd8c9c301
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-05-10 00:36:40 -0300

    .

--- src/main/java/com/ccp/web/spring/exceptions/handler/CcpSyncExceptionHandler.java
+++ src/main/java/com/ccp/web/spring/exceptions/handler/CcpSyncExceptionHandler.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.ccp.constantes.CcpConstants;
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.exceptions.process.CcpFlow;
-import com.ccp.validation.CcpJsonFieldsValidations.CcpJsonInvalid;
+import com.ccp.validation.CcpJsonInvalid;
 
 import jakarta.servlet.http.HttpServletResponse;
 
