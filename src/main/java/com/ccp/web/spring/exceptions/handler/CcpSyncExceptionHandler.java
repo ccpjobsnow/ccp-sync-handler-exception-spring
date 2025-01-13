1b56daa8f049422ef153f0ede33511760f7de3c6
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2025-01-12 21:09:41 -0300

    .

--- src/main/java/com/ccp/web/spring/exceptions/handler/CcpSyncExceptionHandler.java
+++ src/main/java/com/ccp/web/spring/exceptions/handler/CcpSyncExceptionHandler.java
@@ -35,7 +35,7 @@ public class CcpSyncExceptionHandler {
 		
 		String message = e.getMessage();
 		
-		CcpJsonRepresentation result = CcpOtherConstants.EMPTY_JSON.put("msg", message);
+		CcpJsonRepresentation result = CcpOtherConstants.EMPTY_JSON.put("message", message);
 		
 		if(e.fields.length <= 0) {
 			return result.content;
